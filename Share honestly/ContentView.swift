c5091dea3927c19ee468c3024887fbb8972e41d4
Andrey Kudryavtsev <eightkbit@gmail.com>
2020-12-22 21:27:47 +0500

    Initial Commit

---
+++ Share honestly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Share honestly
+//
+//  Created by Andrey Kudryavtsev on 22.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Share honestly/Share_honestlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_honestlyApp.swift
+//  Share honestly
+//
+//  Created by Andrey Kudryavtsev on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Share_honestlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
